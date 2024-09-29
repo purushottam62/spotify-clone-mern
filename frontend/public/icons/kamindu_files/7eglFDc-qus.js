e8c2f73af9b9091ffb1dcb2d52f25806eb60f1c3
Purushottam620xyz <purushottam.lal.cse23@itbhu.ac.in>
2024-09-29 20:00:32 +0530

    final code is ready with all features

--- frontend/public/icons/kamindu_files/7eglFDc-qus.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("PolarisBoostEditAudienceScreenQuery_instagramRelayOperation",[],(function(a,b,c,d,e,f){e.exports="7974265355990397"}),null);
-__d("PolarisStoriesArchiveRoot.entrypoint",["JSResourceForInteraction"],(function(a,b,c,d,e,f,g){"use strict";a={getPreloadProps:function(a){return{queries:{}}},root:c("JSResourceForInteraction")("PolarisStoriesArchiveRoot.react").__setRef("PolarisStoriesArchiveRoot.entrypoint")};g["default"]=a}),98);
